2e4d0d58e02ca3ad64eefe3f20a8b81cf932e19a
Jake <78247679+killerclaws12@users.noreply.github.com>
2021-07-06 19:28:16 -0400

    Update help.js

--- commands/amongus/help.js
+++ commands/amongus/help.js
@@ -1,6 +1,6 @@
 module.exports = {
     name : 'help',
     run : async(client, message, args) => {
-        message.channel.send('Click here for more information on how to use the bot -> https://github.com/reconlx/amongus-discord-bot/blob/main/README.md#-commands')
+        message.channel.send('Click here for more information on how to use the bot -> https://github.com/killerclaws12/amongus-discord/')
     }
 }
